dab7ea7bf953f40e7fc60df9c946e21ff688581a
Stephen Done <srdone@gmail.com>
2015-03-24 13:04:02 -0600

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+
+
+var isPalindrome = function (string) {
+  var reversedString = string.split('').reverse().join('');
+  return string === reversedString;
+};
+
+
+// Tests
+
+assert.equal(
+  isPalindrome('alala'),
+  true
+);
+
+assert.equal(
+  isPalindrome('as'),
+  false
+);
+
+assert.equal(
+  isPalindrome('ala'),
+  true
+);
+
+assert.equal(
+  isPalindrome('a'),
+  true
+);
+
+assert.equal(
+  isPalindrome('aaaaaa'),
+  true
+);
+
+assert.equal(
+  isPalindrome('xxxaxxx'),
+  true
+);
+
+console.log('Success!');
