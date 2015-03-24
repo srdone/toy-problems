351df2176939d114ea12de28bbb6216a1bd96f46
Stephen Done <srdone@gmail.com>
2015-03-24 12:47:30 -0600

    added simple assertion

--- toy-problems.js
+++ toy-problems.js
@@ -252,3 +252,11 @@ Write a sum method which will work properly when invoked using either syntax bel
 console.log(sum(2,3));   // Outputs 5
 console.log(sum(2)(3));  // Outputs 5
 */
+
+
+assert.equal(
+  sum(2,3),
+  sum(2)(3)
+);
+
+console.log('We got it!');
